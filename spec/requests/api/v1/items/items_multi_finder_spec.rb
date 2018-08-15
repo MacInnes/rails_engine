5087286c62aa907ed42fd64de5b2ab7282c38de8
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-14 19:29:49 -0600

    Add ability for users to find all items by unit price.

--- spec/requests/api/v1/items/items_multi_finder_spec.rb
+++ spec/requests/api/v1/items/items_multi_finder_spec.rb
@@ -62,20 +62,27 @@ describe 'Item Multi Finder API' do
       expect(item).to have_key('merchant_id')
     end
 
-    # it 'returns all items by unit price' do
-    #   unit_price = create(:item).unit_price
-    #
-    #   get "/api/v1/items/find_all?unit_price=#{unit_price}"
-    #
-    #   item = JSON.parse(response.body)
-    #
-    #   expect(response).to be_successful
-    #   expect(item['unit_price']).to eq((unit_price.to_f / 100).round(2).to_s)
-    #   expect(item).to have_key('unit_price')
-    #   expect(item).to have_key('description')
-    #   expect(item).to have_key('merchant_id')
-    # end
-    #
+    it 'returns all items by unit price' do
+      valid_unit_price = 75107
+      invalid_unit_price = 67076
+      create_list(:item, 3, unit_price: valid_unit_price)
+      create_list(:item, 3, unit_price: invalid_unit_price)
+
+      get "/api/v1/items/find_all?unit_price=#{valid_unit_price}"
+
+      items = JSON.parse(response.body)
+      item = items.first
+
+      expect(response).to be_successful
+      expect(items.class).to eq(Array)
+      expect(items.count).to eq(3)
+      expect(item['unit_price']).to eq((valid_unit_price.to_f / 100).round(2).to_s)
+      expect(item).to have_key('name')
+      expect(item).to have_key('unit_price')
+      expect(item).to have_key('description')
+      expect(item).to have_key('merchant_id')
+    end
+
     # it 'returns all items by merchant id' do
     #   merchant_id = create(:item).merchant_id
     #
