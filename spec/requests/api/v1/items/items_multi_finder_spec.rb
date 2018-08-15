4390c8c54d9e4e4d8aaa35a938effb95c0aef9a8
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-14 19:28:02 -0600

    Add ability for users to see all items by description.

--- spec/requests/api/v1/items/items_multi_finder_spec.rb
+++ spec/requests/api/v1/items/items_multi_finder_spec.rb
@@ -41,20 +41,27 @@ describe 'Item Multi Finder API' do
       expect(item).to have_key('merchant_id')
     end
 
-    # it 'returns all items by description' do
-    #   description = create(:item).description
-    #
-    #   get "/api/v1/items/find_all?description=#{description}"
-    #
-    #   item = JSON.parse(response.body)
-    #
-    #   expect(response).to be_successful
-    #   expect(item['description']).to eq(description)
-    #   expect(item).to have_key('unit_price')
-    #   expect(item).to have_key('description')
-    #   expect(item).to have_key('merchant_id')
-    # end
-    #
+    it 'returns all items by description' do
+      valid_description = 'Nihil autem sit odio inventore deleniti.'
+      invalid_description = 'Cumque consequuntur ad.'
+      create_list(:item, 3, description: valid_description)
+      create_list(:item, 3, description: invalid_description)
+
+      get "/api/v1/items/find_all?description=#{valid_description}"
+
+      items = JSON.parse(response.body)
+      item = items.first
+
+      expect(response).to be_successful
+      expect(items.class).to eq(Array)
+      expect(items.count).to eq(3)
+      expect(item['description']).to eq(valid_description)
+      expect(item).to have_key('name')
+      expect(item).to have_key('unit_price')
+      expect(item).to have_key('description')
+      expect(item).to have_key('merchant_id')
+    end
+
     # it 'returns all items by unit price' do
     #   unit_price = create(:item).unit_price
     #
