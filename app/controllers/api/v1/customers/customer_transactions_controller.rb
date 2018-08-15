271826830ffc1af355a85e979d13ac70ab3a2fca
MacInnes <califormula@gmail.com>
2018-08-14 18:36:59 -0600

    refactored customer transactions method

--- app/controllers/api/v1/customers/customer_transactions_controller.rb
+++ app/controllers/api/v1/customers/customer_transactions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Customers::CustomerTransactionsController < ApplicationController
 
   def index
     # byebug
-    render json: Customer.includes(:invoices, :transactions).find(params[:customer_id]).transactions
+    render json: Customer.includes(:invoices).find(params[:customer_id]).transactions
   end
 
 end
