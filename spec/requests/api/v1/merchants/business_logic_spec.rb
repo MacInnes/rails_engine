aa149acefc0698bb5d66c0ec60e42905848e6e3b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-15 22:15:35 -0600

    Fixed test to reflect change to finding revenue by date.

--- spec/requests/api/v1/merchants/business_logic_spec.rb
+++ spec/requests/api/v1/merchants/business_logic_spec.rb
@@ -72,25 +72,28 @@ describe "merchant business logic" do
     item_1 = create(:item, merchant_id: merchant_1.id)
     item_2 = create(:item, merchant_id: merchant_2.id)
     item_3 = create(:item, merchant_id: merchant_3.id)
+
+    bad_date = "2017-08-13".to_date
+
     invoice_1 = create(:invoice, customer_id: customer.id, merchant_id: merchant_1.id)
     invoice_2 = create(:invoice, customer_id: customer.id, merchant_id: merchant_2.id)
-    invoice_3 = create(:invoice, customer_id: customer.id, merchant_id: merchant_3.id)
+    invoice_3 = create(:invoice, customer_id: customer.id, merchant_id: merchant_3.id, created_at: bad_date)
+
 
     create_list(:invoice_item, 5, item_id: item_1.id, invoice_id: invoice_1.id)
     create_list(:invoice_item, 3, item_id: item_2.id, invoice_id: invoice_2.id)
     create_list(:invoice_item, 1, item_id: item_3.id, invoice_id: invoice_3.id)
 
-    bad_date = "2017-08-13".to_date.end_of_day
 
     create(:transaction, invoice_id: invoice_1.id)
     create(:transaction, invoice_id: invoice_2.id)
-    create(:transaction, invoice_id: invoice_3.id, created_at: bad_date)
+    create(:transaction, invoice_id: invoice_3.id)
     create(:transaction, result: 'failed', invoice_id: invoice_3.id)
 
     get '/api/v1/merchants/revenue?date=2018-08-13'
 
     response_total = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(response_total.first["total_revenue"]).to eq("800.00")
+    expect(response_total["total_revenue"]).to eq("800.00")
   end
 end
